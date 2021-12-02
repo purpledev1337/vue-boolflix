65dfac09396142982046aab667800f2f64acedb6
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-12-02 17:06:34 +0100

    Feat: added img based on 3 original_language cases

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -7,7 +7,9 @@
             <h1>{{ movie.title }}</h1>
             <h2>{{ movie.vote_average}}</h2>
             <h3>{{ movie.original_title }}</h3>
-            <h4>{{ movie.original_language }}</h4>
+            <img v-if="movie.original_language === 'it'" src="../assets/Flag_ITA.png" :alt="movie.original_language">
+            <img v-if="movie.original_language === 'en'" src="../assets/Flag_UK.png" :alt="movie.original_language">
+            <img v-if="movie.original_language !== 'it' & movie.original_language !== 'en'" class="world_flag" src="../assets/Flag_WORLD.jpg" :alt="movie.original_language">
         </li>
     </ul>
 
@@ -25,5 +27,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+img {
+    width: 30px;
+}
 
 </style>
