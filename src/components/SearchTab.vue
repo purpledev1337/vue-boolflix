49a9d4721011e1bfda63fe0ce4a166634de59fd4
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-12-06 16:44:33 +0100

    MinUpdate: better SearchTab url data management

--- src/components/SearchTab.vue
+++ src/components/SearchTab.vue
@@ -13,6 +13,9 @@ export default {
   data() {
     return {
       searchTxt: '',
+      baseUrl: 'https://api.themoviedb.org/3/search/',
+      apiKey: 'api_key=6daf8c812c8d1160c91335b1fd6ab733',
+      apiLanguage: '&language=it-IT&query=',
       apiUrl: '',
       searchedMoviesList: [],
       searchedSeriesList: []
@@ -20,22 +23,30 @@ export default {
   },
   methods: {
     searchAny() {
-      this.apiUrl = "https://api.themoviedb.org/3/search/movie?api_key=6daf8c812c8d1160c91335b1fd6ab733" + "&language=it-IT" + "&query=" + this.searchTxt.replace(/\s/g,"+");
+
+      this.apiUrl = this.baseUrl + "movie?" + this.apiKey + this.apiLanguage + this.searchTxt.replace(/\s/g,"+");
+
       axios
       .get(this.apiUrl)
       .then((foundMoviesList) => {
+
           this.searchedMoviesList = foundMoviesList.data.results;
           console.log("Lista FILM in SearchTab", this.searchedMoviesList);
           this.$emit('moviesFound', this.searchedMoviesList);
-          });
-      this.apiUrl = "https://api.themoviedb.org/3/search/tv?api_key=6daf8c812c8d1160c91335b1fd6ab733" + "&language=it-IT" + "&query=" + this.searchTxt.replace(/\s/g,"+");
+
+      });
+
+      this.apiUrl = this.baseUrl + "tv?" + this.apiKey + this.apiLanguage + this.searchTxt.replace(/\s/g,"+");
+      
       axios
       .get(this.apiUrl)
       .then((foundSeriesList) => {
+
           this.searchedSeriesList = foundSeriesList.data.results;
           console.log("Lista SERIE in SearchTab", this.searchedSeriesList);
-          this.$emit('seriesFound', this.searchedSeriesList)
-          });
+          this.$emit('seriesFound', this.searchedSeriesList);
+
+      });
     }
   }
 }
